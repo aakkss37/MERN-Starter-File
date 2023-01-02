4933dcd78e879678d1a95d209507e0a98441a805
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-02 13:06:07 +0530

    route control modified

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 import express, { response } from "express";
+import { getPost } from "../controllers/posts.js";
 
 const router = express.Router()
 
-router.get("/", (req, res)=>{
-	res.send("Hello from the server.");
-});
+router.get("/", getPost);
 
 export default router;
